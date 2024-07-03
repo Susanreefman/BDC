459157db374708f87357adae60ae6ca999c8ebed
Susanreefman <74648927+Susanreefman@users.noreply.github.com>
2024-07-03 17:44:01 +0200

    Update assignment3.py

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -1,102 +1,92 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
-Assignment 3: Big Data Computing
+Assignment 3 Big Data Computing
 """
 
+__author__ = "hsreefman"
+
+# Imports
 import sys
-import argparse
-import numpy as np
 
 
-def argument_parser():
-    """create an argument parser object"""
-    parser = argparse.ArgumentParser(
-        description="Script for Assignment 3 of Big Data Computing."
-    )
-    # Create exclusive group for the two modes
-    mode = parser.add_mutually_exclusive_group(required=True)
-    mode.add_argument(
-        "--chunk",
-        action="store_true",
-        help="Run the program in chunk parsing mode;",
-    )
-    mode.add_argument(
-        "--combine",
-        action="store_true",
-        help="Run the program in combine parsing mode;" 
-    )
-    return parser.parse_args()
+CHUNK_SIZE = 60
+
+def get_header(lines):
+    """
+    Determines the header line number
+    Param:
+        lines (list): list with separate lines
+    Return:
+        index (int): index of start position fastq
+    """
+    for index, line in enumerate(lines):
+        if line.startswith("@"):
+            return index
+    return -1
 
 
-def get_quality_line():
-    """Get quality line"""
-    with sys.stdin.buffer as fastq:
-        i = 0
-        while i < 2:
-            # Skip 3 lines
-            if not fastq.readline():
-                i += 1
-            fastq.readline()
-            fastq.readline()
-            # Quality line
-            yield fastq.readline().strip()
+def get_phredscore(chunk_lines):
+    """
+    Calculates the phred mean scores and puts them in a list
+    Param:
+        chunk_lines (string):
+    Return:
+        vertical_list (list):
+    """
+    # Flatten the list of lists and compute the lengths
+    lengths = [len(sublist[0]) for sublist in chunk_lines]
+
+    # Find the maximum length
+    max_length = max(lengths)
+
+    scores = []
+    for i in range(max_length):
+        score = []
+        for line in chunk_lines:
+            line = line[0]
+            if i < len(line):
+                score.append(ord(line[i]) - 33)
+
+            scores.append(sum(score) / len(score))
+
+    return scores
 
 
-def combine_list(array_list, is_phred=False):
-    """Combine list of np arrays"""
-    max_rlength = np.array([len(item) for item in array_list]).max()
-    
-    combine = np.zeros((len(array_list), max_rlength))
+def process_file(filename):
+    """
+    Process files 
+    Param:
+        filename (str): The name of the FASTQ file to be processed.
+    Return:
+        average (float) or None: The average PHRED score if the process rank is 0, otherwise None.
+    """
+    with open(filename, 'r') as f:
+        lines = f.readlines()
 
-    for i, array in enumerate(array_list):
-        combine[i, : len(array)] = array
+    start_index = get_header(lines)
 
-    if is_phred:
-        combine[np.nonzero(combine)] -= 33
-    return combine
+    quality_lines = []
+    for i in range(start_index + 3, len(lines), 4):
+        quality_lines.append(lines[i])
 
+    chunk_lines = []
+    for span in range(0, len(quality_lines), 1):
+        chunk_lines.append(quality_lines[span: span + 1])
 
-def get_sum_phred():
-    """Get sum of phred scores"""
-    quality = [np.frombuffer(line, dtype=np.uint8) for line in get_quality_line()]
-    quality_array = combine_list(quality, is_phred=True)
-    phred_sum = np.sum(quality_array, axis=0)
-    phred_count = np.count_nonzero(quality_array, axis=0)
-    return phred_sum, phred_count
+    scores = get_phredscore(chunk_lines)
 
-
-def main() -> int:
-    """Main"""
-    args = argument_parser()
-
-    # Chunk
-    if args.chunk:
-        sum, count = get_sum_phred()
-        # Stdout
-        print("sum:", list(sum))
-        print("count:", list(count))
-    # Combine
-    elif args.combine:
-        sum_array_list = []
-        count_array_list = []
-        with sys.stdin as lines:
-            for line in lines:
-                if line.startswith("sum:"):
-                    sum_array_list.append(np.fromstring(line.strip()[6:-1], sep=", "))
-                elif line.startswith("count:"):
-                    count_array_list.append(np.fromstring(line.strip()[8:-1], sep=", "))
-        # Combine the arrays
-        sum_total = np.sum(combine_list(sum_array_list), axis=0)
-        count_total = np.sum(combine_list(count_array_list), axis=0)
-        
-        # Calculate and stdout avg
-        avg = np.divide(sum_total, count_total, dtype=np.float64)
-        for i, value in enumerate(avg):
-            print(f"{i},{value}")
-
-    return 0
+    return scores
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    if len(sys.argv) != 2:
+        print("Usage: python assignment3.py <input_fastq_file>")
+        sys.exit(1)
+
+    input_fastq = sys.argv[1]
+
+    avg_phred = process_file(input_fastq)
+
+    print("\n".join([f"{index}, {score}" for index, score in enumerate(avg_phred)]))
+    
