90e7892cdb3acb97fc18abc7d021414274ac2791
Susanreefman <74648927+Susanreefman@users.noreply.github.com>
2024-07-04 09:44:36 +0200

    Update assignment3.sh

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -13,7 +13,4 @@ source /commons/conda/conda_load.sh;
 export FILE1="/data/datasets/rnaseq_data/Brazil_Brain/SPM11_R1.fastq"
 export FILE2="/data/datasets/rnaseq_data/Brazil_Brain/SPM14_R1.fastq"
 
-# export FILE1="test.fastq"
-# export FILE2="testt.fastq"
-
 parallel -j 20 python assignment3.py {} ::: $FILE1 $FILE2
